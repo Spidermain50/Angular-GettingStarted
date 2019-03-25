47c44feb2848afa1e28ca99564462a86092ed33a
Spidermain50 <spidermain50@gmail.com>
2019-03-25 08:20:42 -0400

    11-7: Placing the Views

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 
-@Component ({
+@Component({
   selector: 'pm-root',
   template: `
   <nav class='navbar navbar-expand navbar-light bg-light'>
-  <a class='navbar-brand'>{{pageTitle}}</a>
-  <ul class='nav nav-pills'>
-    <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
-    <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Product List</a></li>
-  </ul>
-</nav>
+    <a class='navbar-brand'>{{pageTitle}}</a>
+    <ul class='nav nav-pills'>
+      <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
+      <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Product List</a></li>
+    </ul>
+  </nav>
+  <div class='container'>
+        <router-outlet></router-outlet>
+  </div>
   `
 })
 export class AppComponent {
